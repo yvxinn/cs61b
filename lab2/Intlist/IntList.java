2446966d5370eddbdfcc5b5761e448b8c988cf49
yvxin <wangyv_x@163.com>
2023-12-13 21:05:59 +0800

    finish lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,6 +83,8 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         IntList p=A;
+        if(p==null)
+            return B;
         while(p.rest!=null)
             p=p.rest;
         p.rest=B;
