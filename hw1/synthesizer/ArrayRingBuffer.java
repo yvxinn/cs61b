b6ced7d83d8bdd25dded03f24deaf7ac184b7996
yvxin <wangyv_x@163.com>
2023-12-25 19:56:57 +0800

    finish hw1 1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -42,6 +42,9 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     }
     @Override
     public T peek() {
+        if(isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
         return rb[first];
     }
     private class ArrayRingBufferIterator implements Iterator<T>{
